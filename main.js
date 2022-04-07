e40508136f60ffcdeb8a46b7d0b072a9097a0379
SirAlyon <alessiosada04@gmail.com>
2022-04-07 16:53:20 +0200

    add bonus 2

--- main.js
+++ main.js
@@ -35,13 +35,34 @@ for (let key in team){
     console.log(team[key]);
 }
 
-for (let index = 0; index < team.length; index++) {
+/* for (let index = 0; index < team.length; index++) {
     const person = team[index];
+    const personDivElement = document.createElement()
+    document.querySelector('.team').insertAdjacentHTML('beforeend', `<img src="./img/${person.image}"></img>`)
+
     document.querySelector('.team').insertAdjacentHTML('beforeend', `<div>${person.name}</div>` )
     document.querySelector('.team').insertAdjacentHTML('beforeend', `<div>${person.role}</div>`)
-    document.querySelector('.team').insertAdjacentHTML('beforeend', `<img src="./img/${person.image}"></img>`)
 }
-
+ */
 /* console.log(team[0].name);
 document.querySelector('.team').insertAdjacentHTML('beforeend', team[0].name) */
 
+for (let index = 0; index < team.length; index++) {
+    const person = team[index];
+    const ourTeam = document.querySelector('.team')
+    const personDivElement = document.createElement('div')
+    ourTeam.append(personDivElement)
+    personDivElement.classList.add('person')
+    personDivElement.innerHTML = `<img src="./img/${person.image}"></img>
+                                    <div> 
+                                        <span class="fw-bold fs-5">${person.name}</span> 
+                                        <span>${person.role}</span>
+                                    </div>`
+
+
+
+/*     document.querySelector('.team').insertAdjacentHTML('beforeend', `<img src="./img/${person.image}"></img>`)
+
+    document.querySelector('.team').insertAdjacentHTML('beforeend', `<div>${person.name}</div>` )
+    document.querySelector('.team').insertAdjacentHTML('beforeend', `<div>${person.role}</div>`) */
+}
